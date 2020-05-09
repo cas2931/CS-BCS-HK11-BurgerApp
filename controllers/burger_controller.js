bbfe2ab82ae60750049dc43b3ac69e9279ff562f
Silver Snail 57 <cdarkly1392@gmail.com>
2020-05-09 09:35:17 -0400

    Setup controller and views folders

---
+++ controllers/burger_controller.js
@@ -0,0 +1,35 @@
+var burger = require('../models/burger.js') 
+var express = require('express'); 
+var router = express.router(); 
+
+router.get('/', function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        //console.log(hbsObject);
+        res.render('index',hbsObject);
+    });
+});
+
+router.post('/burgers', function(req, res) {
+    burger.insertOne([
+        'burger_name'
+    ],
+    [
+        req.body.burger_name
+    ], function(data) {
+        res.redirect('/');
+    });
+}); 
+
+router.put('/burgers/:id', function(req, res) {
+    var condition = 'id = ' + req.params.id;
+    burger.updateOne({
+        devoured: true
+    }, condition, function(data) {
+        res.redirect('/');
+    });
+}); 
+
+module.exports = router;
