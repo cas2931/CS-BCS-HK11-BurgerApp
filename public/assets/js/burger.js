f9996cc4e0020956ee8416684bea122294ca6e1e
Silver Snail 57 <cdarkly1392@gmail.com>
2020-05-09 22:49:11 -0400

    Updated html and UI. Ready to deploy to heroku

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,21 +1,21 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $("#devourButton").on("click", function(event) {
-    var id = $(this).data("id");
-    var newEaten = $(this).data("newEaten");
+// $(function() {
+//   $(".btn btn-success").on("click", function(event) {
+//     var id = $(this).data("id");
+//     var newEaten = $(this).data("newEaten");
 
-    var newEatenState = {
-      devoured: true
-    };
+//     var newEatenState = {
+//       devoured: newEaten
+//     };
 
-    // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newEatenState
-    }).then(
-      function() {
-        location.reload();
-      }
-    );
-  }); 
-})
+//     // Send the PUT request.
+//     $.ajax("/api/burgers/" + id, {
+//       type: "PUT",
+//       data: newEatenState
+//     }).then(
+//       function() {
+//         location.reload();
+//       }
+//     );
+//   }); 
+// })
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var PORT = process.env.PORT || 8080;
-var app = express(); 
+var app = express();  
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var exphbs = require("express-handlebars"); 
 var routes = require('./controllers/burger_controller.js'); 
 
 app.use(express.static("public"));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json()); 
+app.use(methodOverride('_method'));
 
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
